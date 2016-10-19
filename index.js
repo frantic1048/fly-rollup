7dab6b39e198ae0500c6468f27de7beb76835e8f
frantic1048 <archer@frantic1048.com>
2016-10-19 19:59:53 +0800

    add sourcemap support, fixes #1

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ module.exports = function flyRollup () {
     return rollup.rollup(rollupOpt)
       .then(function (bundle) {
         const result = bundle.generate(bundleOpt)
+        const code = result.code.toString()
+
+        let output = code
+
+        if (bundleOpt.sourceMap) {
+          const map = result.map.toString()
+          output += `\n//# sourceMappingURL=data:application/json;base64,${new Buffer(map).toString('base64')}`
+        }
         return {
           ext: '.js',
-          code: result.code.toString(),
-          map: result.map
+          code: output
         }
       })
   })
