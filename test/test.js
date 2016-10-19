3467ad105fb5eacb2ebc2c94222c986dfea047fb
frantic1048 <archer@frantic1048.com>
2016-10-19 20:20:38 +0800

    fix weird ' escaping on CI

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ test('bundle', async t => {
   }
   const result = await fly.rollup(data, options)
   t.is(result.ext, '.js')
-  t.is(result.code, 'var module = (function () {\n\'use strict\';\n\nfunction a () {\n  return \'a\';\n}\n\nfunction b () {\n  return \'b\';\n}\n\nfunction entry () {\n  a();\n  b();\n}\n\nreturn entry;\n\n}());')
+  t.is(result.code, "var module = (function () {\n'use strict';\n\nfunction a () {\n  return 'a';\n}\n\nfunction b () {\n  return 'b';\n}\n\nfunction entry () {\n  a();\n  b();\n}\n\nreturn entry;\n\n}());")
 })
 
 test('bundle with sourceMap', async t => {
@@ -45,5 +45,5 @@ test('bundle with sourceMap', async t => {
   }
   const result = await fly.rollup(data, options)
   t.is(result.ext, '.js')
-  t.is(result.code, 'var module = (function () {\n\'use strict\';\n\nfunction a () {\n  return \'a\';\n}\n\nfunction b () {\n  return \'b\';\n}\n\nfunction entry () {\n  a();\n  b();\n}\n\nreturn entry;\n\n}());\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjpudWxsLCJzb3VyY2VzIjpbIi92YXIvc29yYS9jb2RlL2ZseS1yb2xsdXAvdGVzdC9maXh0dXJlcy9hLmpzIiwiL3Zhci9zb3JhL2NvZGUvZmx5LXJvbGx1cC90ZXN0L2ZpeHR1cmVzL2IuanMiLCIvdmFyL3NvcmEvY29kZS9mbHktcm9sbHVwL3Rlc3QvZml4dHVyZXMvZW50cnkuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKCkge1xuICByZXR1cm4gJ2EnXG59XG4iLCJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiAoKSB7XG4gIHJldHVybiAnYidcbn1cbiIsImltcG9ydCBhIGZyb20gJy4vYSdcbmltcG9ydCBiIGZyb20gJy4vYidcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKCkge1xuICBhKClcbiAgYigpXG59XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsY0FBMkI7U0FDbEIsR0FBUDs7O0FDREYsY0FBMkI7U0FDbEIsR0FBUDs7O0FDRUYsa0JBQTJCOzs7Ozs7OyJ9')
+  t.is(result.code, "var module = (function () {\n'use strict';\n\nfunction a () {\n  return 'a';\n}\n\nfunction b () {\n  return 'b';\n}\n\nfunction entry () {\n  a();\n  b();\n}\n\nreturn entry;\n\n}());\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjpudWxsLCJzb3VyY2VzIjpbIi92YXIvc29yYS9jb2RlL2ZseS1yb2xsdXAvdGVzdC9maXh0dXJlcy9hLmpzIiwiL3Zhci9zb3JhL2NvZGUvZmx5LXJvbGx1cC90ZXN0L2ZpeHR1cmVzL2IuanMiLCIvdmFyL3NvcmEvY29kZS9mbHktcm9sbHVwL3Rlc3QvZml4dHVyZXMvZW50cnkuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKCkge1xuICByZXR1cm4gJ2EnXG59XG4iLCJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiAoKSB7XG4gIHJldHVybiAnYidcbn1cbiIsImltcG9ydCBhIGZyb20gJy4vYSdcbmltcG9ydCBiIGZyb20gJy4vYidcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKCkge1xuICBhKClcbiAgYigpXG59XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsY0FBMkI7U0FDbEIsR0FBUDs7O0FDREYsY0FBMkI7U0FDbEIsR0FBUDs7O0FDRUYsa0JBQTJCOzs7Ozs7OyJ9")
 })
