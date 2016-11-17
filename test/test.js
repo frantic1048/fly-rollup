d6ad187c898dc66f7e0eb2c61bdd2800d3ffa532
Luke Edwards <luke.edwards05@gmail.com>
2016-11-17 10:57:47 -0800

    fix tests

--- test/test.js
+++ test/test.js
@@ -32,17 +32,21 @@ const fly = new Fly({
       const res = yield this.$.read(`${tmp}/entry.js`, 'utf8');
       t.ok(res, 'writes output file');
       t.equal(res, want, 'produces correct content');
+
+      yield this.clear(tmp);
     },
     b: function * (o) {
       const t = o.val;
 
       const opts = getOps();
       opts.rollup.entry = `${dir}/entry.js`;
-      yield this.source(`${tmp}/*.foo`).rollup(opts).target(tmp);
+      yield this.source(`${dir}/*.foo`).rollup(opts).target(tmp);
 
       const res = yield this.$.read(`${tmp}/entry.js`, 'utf8');
       t.ok(res, 'writes output file');
       t.equal(res, want, 'produces correct content');
+
+      yield this.clear(tmp);
     },
     c: function * (o) {
       const t = o.val;
@@ -55,27 +59,14 @@ const fly = new Fly({
       t.ok(res, 'writes output file');
       t.true(res.indexOf(want) > -1, 'produces correct content');
       t.true(res.indexOf('# sourceMappingURL=data:application/json;base64,{"version":3,') > -1, 'appends `sourceMappingURL` content');
+
+      yield this.clear(tmp);
     }
   }
 });
 
-const co = fly.$.coroutine;
-
-test('fly-rollup: source', co(function * (t) {
-  t.plan(3);
+test('fly-rollup', t => {
+  t.plan(8);
   t.true('rollup' in fly, 'attach `rollup()` plugin to fly');
-  yield fly.start('a', {val: t});
-  yield fly.clear(tmp);
-}));
-
-test('fly-rollup: opts.entry', co(function * (t) {
-  t.plan(2);
-  yield fly.start('b', {val: t});
-  yield fly.clear(tmp);
-}));
-
-test('fly-rollup: sourceMap', co(function * (t) {
-  t.plan(3);
-  yield fly.start('c', {val: t});
-  yield fly.clear(tmp);
-}));
+  fly.serial(['a', 'b', 'c'], {val: t});
+});
